154d330570a899a46cdca024c49a00bcb0de0f4a
Akshay Bhatt <120771512+Akshay18bhatt@users.noreply.github.com>
2024-02-28 08:03:53 +0000

    t1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,36 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
+
+
+const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"]
 const App = () => {
+
+
+  
+  const [fruitArr, setFruitArr]= useState(fruits);
+
+    function filterFruits(e){
+      let filteredArr= fruits.filter(fruit=>fruit.includes(e.target.value));
+      setFruitArr(filteredArr)
+    }
+
+  
   return (
-    <div>
-        {/* Do not remove the main div */}
+
+    
+    <div id="main">
+        
+        <div>
+          <h1>Search item</h1>
+          <input onChange={filterFruits} placeholder="search fruit"></input>
+          <ul>
+            {fruitArr.map(fruit=>{
+              return(<li key={fruit} >{fruit}</li>)
+            })}
+          </ul>
+        </div>
     </div>
   )
 }
